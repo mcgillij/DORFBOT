3b4c74ede45c1bbdadd9385dda6677be8a57e560
mcgillij <mcgillivray.jason@gmail.com>
2023-04-05 22:30:29 -0300

    removed some debugging

--- dorfbot.py
+++ dorfbot.py
@@ -28,19 +28,16 @@ async def on_message(message):
         logging.info(f'Response data: {data["response"]}')
         if data['response'] == '\n\n<end>':
             result_string = ''.join(results).replace(default_prompt, '').replace(query, '')
-            print(result_string)
             results.clear()
             asyncio.run_coroutine_threadsafe(message.channel.send(result_string), client.loop)
         else:
             logging.info(f'{data}')
             results.append(data['response'])
     # ignore self messages
-    print(message.content)
     if message.author == client.user:
         return
 
     if ('!Dorf') in message.content:
-        print('Dorfdd')
         query = message.content.replace('!Dorf', '')
         prompt = f"{default_prompt}{query} is"
 
@@ -56,9 +53,6 @@ async def on_message(message):
         }
         sio.emit('request', req, callback=lambda data: on_result(data, message, query))
         sio.on('result', lambda data: on_result(data, message, query))
-
-    # Define a callback function to handle the 'result' event
-
 
 # Wait for responses from the server
 sio.start_background_task(sio.wait)
